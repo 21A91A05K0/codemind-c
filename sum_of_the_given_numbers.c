b360656ed2232c07944ae761daedb1d57f400eb4
21A91A05K0 <97424441+21A91A05K0@users.noreply.github.com>
2022-03-10 10:04:42 +0530

    #include<stdio.h> int main() {     int i,n,a[n],b[n];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d%d",&a[i],&b[i]);     }     for(i=0;i<n;i++)     {         printf("%d ",a[i]+b[i]);     }     return 0; }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,a[n],b[n];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d%d",&a[i],&b[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d
+",a[i]+b[i]);
+    }
+    return 0;
+}

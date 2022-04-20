4c8a4460bc0ef79b5ffebc69a10d12340687b458
21A91A05K0 <97424441+21A91A05K0@users.noreply.github.com>
2022-04-20 09:49:35 +0530

    #include<stdio.h> int main() {     int n,i=0,d[100],j;     scanf("%d",&n);     while(n>0)     {         d[i]=n%10;         n=n/10;         i++;     }     for(j=i-1;j>=0;j--)     {         if(d[j]==6)         {             d[j]=9;             break;         }     }     for(int k=i-1;k>=0;k--)     {         printf("%d",d[k]);     } }

---
+++ Maximum_69_Number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=0,d[100],j;
+    scanf("%d",&n);
+    while(n>0)
+    {
+        d[i]=n%10;
+        n=n/10;
+        i++;
+    }
+    for(j=i-1;j>=0;j--)
+    {
+        if(d[j]==6)
+        {
+            d[j]=9;
+            break;
+        }
+    }
+    for(int k=i-1;k>=0;k--)
+    {
+        printf("%d",d[k]);
+    }
+}

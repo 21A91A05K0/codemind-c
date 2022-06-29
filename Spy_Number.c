ecc8bf5542fc1f55a13e3e8a5f8e69ece3a2b1f7
21A91A05K0 <97424441+21A91A05K0@users.noreply.github.com>
2022-06-29 20:30:44 +0530

    #include<stdio.h> int main() { 	int n,i,c=0,m=1; 	scanf("%d",&n); 	for(i=1;n>0;) 	{ 	  i=n%10; 	  n=n/10;
    	  c=c+i;
    	  m=m*i;
    	}
    	if(m==c)
    	{
    		printf("Spy Number");
    	}
    	else
    	{
    		printf("Not Spy Number");
    	}
    }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int n,p=1,sum=0,r;
-    scanf("%d",&n);
-    while(n!=0)
-    {
-        r=n%10;
-        sum=sum+r;
-        p=p*r;
-        n=n/10;
-    }
-    if(sum==p)
-    {
-        printf("Spy Number");
-    }
-    else
-    {
-        printf("Not Spy Number");
-    }
-    return 0;
+	int n,i,c=0,m=1;
+	scanf("%d",&n);
+	for(i=1;n>0;)
+	{
+	  i=n%10;
+	  n=n/10;
+	
+	  c=c+i;
+	  m=m*i;
+	}
+	if(m==c)
+	{
+		printf("Spy Number");
+	}
+	else
+	{
+		printf("Not Spy Number");
+	}
 }

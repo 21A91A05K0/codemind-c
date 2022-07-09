dec836d286025ddb1271dc897755d5d88588ee2b
21A91A05K0 <97424441+21A91A05K0@users.noreply.github.com>
2022-07-09 15:19:51 +0530

    #include<stdio.h> int main() {     int n,c,i,b,a[100],x=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d%d",&b,&c);     for(i=0;i<n;i++)     {         if(b<=a[i] && a[i]<=c || c<=a[i] && a[i]<=b)         {             printf("%d ",a[i]);             x++;         }     }     if(x==0)     {         printf("-1");     } }

--- elements_in_between_A_and_B.c
+++ elements_in_between_A_and_B.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,c,b,a[100],x=0;
+    int n,c,i,b,a[100],x=0;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
-    scanf("%d %d",&b,&c);
+    scanf("%d%d",&b,&c);
     for(i=0;i<n;i++)
     {
         if(b<=a[i] && a[i]<=c || c<=a[i] && a[i]<=b)
